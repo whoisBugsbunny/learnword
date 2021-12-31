5be1b9eec311e3c2f680a19412d855f4a633383b
whoisBugsbunny <whoisBugsbunny>
2021-12-31 23:49:36 +0530

    c4.navwork

--- learnword.js
+++ learnword.js
@@ -7,20 +7,25 @@ const testdiv = document.getElementById('testdiv');
 const navlist = document.querySelectorAll('.navlist li');
 var g_word = "";
 let diction = [];
-$.getJSON("diction.json", function(obj) {
-    // wordarr = obj[0];
-    wordarr = obj[Math.floor((Math.random() * obj.length))];
-    console.log(obj.length);
-    console.log(wordarr);
-    g_word = wordarr.word;
-    word.innerHTML = wordarr.word;
-    def.innerHTML = `<p>${wordarr.definition[0]}</p><p>${wordarr.definition[1]}</p>`;
-    ex.innerHTML = `<p>${wordarr.example[0]}</p><p>${wordarr.example[1]}</p>`;
-    for (let i = 0; i < wordarr.synonyms.length; i++) {
-        const ele = wordarr.synonyms[i];
-        $('#synonyms').append("<div class='synon'>" + ele + "</div> ");
-    }
-});
+
+function randomword() {
+    $.getJSON("diction.json", function(obj) {
+        // wordarr = obj[0];
+        wordarr = obj[Math.floor((Math.random() * obj.length))];
+        console.log(obj.length);
+        console.log(wordarr);
+        g_word = wordarr.word;
+        word.innerHTML = wordarr.word;
+        def.innerHTML = `<p>${wordarr.definition[0]}</p><p>${wordarr.definition[1]}</p>`;
+        ex.innerHTML = `<p>${wordarr.example[0]}</p><p>${wordarr.example[1]}</p>`;
+        synonyms.innerHTML = '';
+        for (let i = 0; i < wordarr.synonyms.length; i++) {
+            const ele = wordarr.synonyms[i];
+            $('#synonyms').append("<div class='synon'>" + ele + "</div> ");
+        }
+    });
+}
+randomword();
 
 function closenav() {
     const pg = document.querySelector(".navpg");
@@ -34,6 +39,41 @@ function wordTest() {
 navlist.forEach(ele => {
     ele.addEventListener("click", closenav);
 });
+const about = document.querySelector("#about");
+const wordpage = document.querySelector("#wordpage");
+const allwords = document.querySelector(".allwords");
+
+navlist[0].addEventListener("click", () => {
+    worddiv.classList.remove("d-none");
+    wordpage.classList.add("d-none");
+    about.classList.add("d-none");
+});
+navlist[1].addEventListener("click", () => {
+    worddiv.classList.add("d-none");
+    wordpage.classList.remove("d-none");
+    about.classList.add("d-none");
+    $.getJSON("diction.json", function(obj) {
+        wordarr = obj[Math.floor((Math.random() * obj.length))];
+        allword = [];
+        obj.forEach(ele => {
+            allword.push(" " + ele.word);
+        });
+        console.log(obj.length);
+        allwords.innerHTML = allword + ".";
+    });
+});
+
+navlist[2].addEventListener("click", () => {
+    worddiv.classList.add("d-none");
+    wordpage.classList.add("d-none");
+    about.classList.remove("d-none");
+});
+navlist[3].addEventListener("click", () => {
+    console.log("not working");
+});
+
+const nextonebtn = document.querySelector('#nextonebtn');
+nextonebtn.addEventListener("click", randomword);
 
 const speakericon = document.querySelector('.speakericon');
 speakericon.addEventListener("click", () => {
